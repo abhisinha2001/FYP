3b7b1a4095ad5bfc0b9073b0e3329e512975b19e
Abhinav Sinha <abhinavsinha@Abhinavs-MacBook-Air.local>
2023-02-08 13:24:45 +0000

    Connection to databse and database setup

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.set('strictQuery',false);
+mongoose.connect('mongodb+srv://admin:Something12345@cluster0.tzrbc9a.mongodb.net/?retryWrites=true&w=majority')
+.then(()=>app.listen(3000))
+.then(()=>console.log("Connected to Database"))
+.catch((err)=>console.log(err));
 
 app.get('/',(req,res)=>{
     res.send('We are at home');
 })
 //Routes
 
-app.listen(3000);
