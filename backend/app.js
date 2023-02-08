a9af00bf7c900d2352f145f407f00ddfdb533fae
Abhinav Sinha <abhinavsinha@Abhinavs-MacBook-Air.local>
2023-02-08 13:32:38 +0000

    Added layer of security to application

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const config = require('dotenv').config();
 const app = express();
 
+const PASSWORD = process.env.PASSWORD
+
 mongoose.set('strictQuery',false);
-mongoose.connect('mongodb+srv://admin:Something12345@cluster0.tzrbc9a.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(`mongodb+srv://admin:`+PASSWORD+`@cluster0.tzrbc9a.mongodb.net/?retryWrites=true&w=majority`)
 .then(()=>app.listen(3000))
 .then(()=>console.log("Connected to Database"))
 .catch((err)=>console.log(err));
