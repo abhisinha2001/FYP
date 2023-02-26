d45e6a0be47078a9729c645eab7dd3fcec484143
Abhinav Sinha <abhinavsinha@Abhinavs-MacBook-Air.local>
2023-02-26 20:42:42 +0000

    Added logo,name, Google Maps Dependencies and tested with sample home page application

--- frontend/lib/pages/HomePage.dart
+++ frontend/lib/pages/HomePage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+const LatLng currentLocation = LatLng(53.350357, -6.266422);
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,14 +11,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late GoogleMapController mapController;
+
+  Map<String, Marker> _markers = {};
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
-        body: Center(
-          child: Text("Hello World"),
+        body: GoogleMap(
+          initialCameraPosition: const CameraPosition(
+            target: currentLocation,
+            zoom: 18,
+          ),
+          onMapCreated: (controller) {
+            mapController = controller;
+            addMarker("test", currentLocation);
+          },
+          markers: _markers.values.toSet(),
         ),
       ),
     );
+  }
+
+  addMarker(String id, LatLng location) {
+    var marker = Marker(
+      markerId: MarkerId(id),
+      position: location,
+    );
+    _markers[id] = marker;
+    setState(() {});
   }
 }
