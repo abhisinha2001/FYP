499e92736e6cd9df6beb0ab89cd612643d959d3a
Abhinav Sinha <abhinavsinha@Abhinavs-MacBook-Air.local>
2023-03-09 15:28:54 +0000

    Added post route to handle images

--- backendCrowdSource/app.js
+++ backendCrowdSource/app.js
@@ -1,6 +1,22 @@
 const express = require("express");
+const fileUpload = require("express-fileupload");
 
 const app = express();
+
+app.use(fileUpload());
+
+// app.use(fileUpload({limits:{fileSize:10000000},abortOnLimit:true}))
+
+app.use(express.static('public'));
+
+app.post('/upload',(req,res)=>{
+    const {image} = req.files;
+    if(!image) return res.sendStatus(400);
+
+    image.mv(__dirname+'/upload/'+image.name);
+    res.sendStatus(200);
+
+});
 
 app.get('/',(req,res)=>{
     res.send("We are at home");
