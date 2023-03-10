ab9654e4436ecc581f5fcb27caba0f2c6416ec72
Abhinav Sinha <abhinavsinha@Abhinavs-MacBook-Air.local>
2023-03-10 16:49:27 +0000

    Corrected backend code and Json Parser for application

--- frontend/lib/models/CCTVModel.dart
+++ frontend/lib/models/CCTVModel.dart
@@ -1,17 +1,51 @@
+// To parse this JSON data, do
+//
+//     final CCTVModel = CCTVModelFromJson(jsonString);
+
+import 'dart:convert';
+
+List<CCTVModel> CCTVModelFromJson(String str) =>
+    List<CCTVModel>.from(json.decode(str).map((x) => CCTVModel.fromJson(x)));
+
+String CCTVModelToJson(List<CCTVModel> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
 class CCTVModel {
-  num cctv_id;
-  String cctv_road;
+  CCTVModel({
+    required this.cctvId,
+    required this.cctvRoad,
+    required this.lat,
+    required this.long,
+    this.policyUrl = "https://www.google.com.ar/",
+    this.phone = "+999999",
+    this.email = "example@somewhere.com",
+  });
+
+  int cctvId;
+  String cctvRoad;
   double lat;
   double long;
-  String policy_url;
-  String email;
-  var photos;
-  var permissions;
+  String? policyUrl;
+  String? phone;
+  String? email;
 
-  CCTVModel(this.cctv_id, this.lat, this.long,
-      {this.policy_url = "default",
-      this.email = "default",
-      this.photos = "default",
-      this.permissions = "default",
-      this.cctv_road = "default"});
+  factory CCTVModel.fromJson(Map<String, dynamic> json) => CCTVModel(
+        cctvId: json["cctv_id"],
+        cctvRoad: json["cctv_road"],
+        lat: json["lat"]?.toDouble(),
+        long: json["long"]?.toDouble(),
+        policyUrl: json["policy_url"],
+        phone: json["Phone"],
+        email: json["Email"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "cctv_id": cctvId,
+        "cctv_road": cctvRoad,
+        "lat": lat,
+        "long": long,
+        "policy_url": policyUrl,
+        "Phone": phone,
+        "Email": email,
+      };
 }
--- frontend/lib/pages/HomePage.dart
+++ frontend/lib/pages/HomePage.dart
@@ -4,8 +4,11 @@ import 'package:frontend/pages/MyPhotoPage.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:url_launcher/url_launcher_string.dart';
+import 'package:http/http.dart' as http;
 
 LatLng currentLocation = LatLng(53.333965, -6.263233);
+
+var URL = "https://3fbf-89-101-60-203.eu.ngrok.io/allcctvs";
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -25,8 +28,13 @@ class _HomePageState extends State<HomePage> {
 
   double _infoWindowPosition = -300;
 
-  CCTVModel _currentCCTVData =
-      CCTVModel(52, 53.350357, -6.266422, cctv_road: "Test");
+  CCTVModel _currentCCTVData = CCTVModel(
+      cctvId: 52,
+      cctvRoad: "Test",
+      lat: 53.350357,
+      long: -6.266422,
+      email: "",
+      phone: "");
 
   @override
   void initState() {
@@ -37,11 +45,121 @@ class _HomePageState extends State<HomePage> {
     setCCTVs();
   }
 
-  void setCCTVs() {
-    CCTVData.add(CCTVModel(39, 53.333965, -6.263233, cctv_road: "Parnell St "));
-    CCTVData.add(CCTVModel(52, 53.350357, -6.266422, cctv_road: "Parnell St "));
-    CCTVData.add(
-        CCTVModel(319, 53.34711, -6.261015, cctv_road: "Batchlors Walk "));
+  Future setCCTVs() async {
+    // var client = http.Client();
+
+    // var uri = Uri.parse(URL);
+    // var response = await client.get(uri);
+
+    // if (response.statusCode == 200) {
+    //   var json = response.body;
+    //   CCTVData.addAll(CCTVModelFromJson(json));
+    // }
+    // {
+    //  "cctv_id": 39,
+    //  "cctv_road": "Harcourt St ",
+    //  "lat": 53.333965,
+    //  "long": -6.263233,
+    //  "policy_url": "https:\/\/www.cctvireland.ie\/pub\/pdf\/Garda-Guidlines-on-CCTV-in-Ireland.pdf",
+    //  "Phone": "01 6663805",
+    //  "Email": null,
+    //  "photos": null,
+    //  "permissions": {"retention":"28","storage":true,"facial_recognition":true}
+    // },
+    //
+
+    CCTVData.add(CCTVModel(
+        cctvId: 39,
+        lat: 53.333965,
+        long: -6.263233,
+        cctvRoad: "Harcourt St ",
+        email: "Not Available",
+        phone: "Not Available",
+        policyUrl: "https:\/\/www.richmond.gov.uk\/cctv"));
+
+    CCTVData.add(CCTVModel(
+        cctvId: 52,
+        lat: 53.350357,
+        long: -6.266422,
+        cctvRoad: "Parnell St ",
+        email: "info@odce.ie",
+        phone: "Not Available",
+        policyUrl: "https:\/\/www.richmond.gov.uk\/cctv"));
+
+    CCTVData.add(CCTVModel(
+        cctvId: 203,
+        lat: 53.330227,
+        long: -6.264374,
+        cctvRoad: "Richmond St South ",
+        email: "Not Available",
+        phone: "Not Available",
+        policyUrl: "https:\/\/www.richmond.gov.uk\/cctv"));
+
+    CCTVData.add(CCTVModel(
+        cctvId: 204,
+        lat: 53.330338,
+        long: -6.259884,
+        cctvRoad: "Ranelagh Rd ",
+        email: "Not Available",
+        phone: "Not Available",
+        policyUrl: "https:\/\/www.richmond.gov.uk\/cctv"));
+
+    CCTVData.add(CCTVModel(
+        cctvId: 218,
+        lat: 53.330822,
+        long: -6.258903,
+        cctvRoad: "Ranelagh Rd ",
+        email: "res.charlemont@claytonhotels.com",
+        phone: "+353 (0)1 960 6700",
+        policyUrl:
+            "https:\/\/www.claytonhotelcharlemont.com\/privacy-statement\/"));
+
+    CCTVData.add(CCTVModel(
+        cctvId: 237,
+        lat: 53.341104,
+        long: -6.250811,
+        cctvRoad: "Merrion Sq North  \/ Merrion Sq West",
+        email: "Not Available",
+        phone: "+ 353 1 661 5133",
+        policyUrl:
+            "https:\/\/www.nationalgallery.ie\/what-we-do\/governance\/privacy-and-data-protection\/privacy-notice"));
+
+    CCTVData.add(CCTVModel(
+        cctvId: 276,
+        lat: 53.337704,
+        long: -6.262714,
+        cctvRoad: "Stephen's Green ",
+        email: "info@stephensgreen.com",
+        phone: "+353 (01) 4780888",
+        policyUrl: "https:\/\/stephensgreen.com\/privacy-policy\/"));
+
+    CCTVData.add(CCTVModel(
+        cctvId: 282,
+        lat: 53.341327,
+        long: -6.258309,
+        cctvRoad: "Dawson Street",
+        email: "dataprotection@ria.ie",
+        phone: "00 353 1 6762570",
+        policyUrl: "https:\/\/www.richmond.gov.uk\/cctv"));
+
+    CCTVData.add(CCTVModel(
+        cctvId: 286,
+        lat: 53.344643,
+        long: -6.259576,
+        cctvRoad: "College Green",
+        email: "dataprotection@tcd.ie",
+        phone: "Not Available",
+        policyUrl:
+            "https:\/\/www.tcd.ie\/about\/policies\/cctv-policy.php#systems"));
+
+    CCTVData.add(CCTVModel(
+        cctvId: 319,
+        lat: 53.34711,
+        long: -6.261015,
+        cctvRoad: "College Green",
+        email: "Not Available",
+        phone: "Not Available",
+        policyUrl: "https:\/\/www.richmond.gov.uk\/cctv"));
   }
 
   Future<bool> _handleLocationPermission() async {
@@ -151,19 +269,69 @@ class _HomePageState extends State<HomePage> {
                         )
                       ]),
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
+                    // crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      Text(_currentCCTVData.cctv_road),
-                      Text(_currentCCTVData.cctv_id.toString()),
-                      Text(_currentCCTVData.lat.toString()),
-                      Text(_currentCCTVData.long.toString()),
+                      Container(
+                          padding: EdgeInsets.only(left: 20),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Wrap(
+                                alignment: WrapAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(bottom: 6),
+                                    child: Text(
+                                      _currentCCTVData.cctvRoad,
+                                      style: TextStyle(
+                                        fontSize: 30,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    "Email:  ",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20),
+                                  ),
+                                  Text(
+                                    _currentCCTVData.email.toString(),
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    "Phone:  ",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20),
+                                  ),
+                                  Text(
+                                    _currentCCTVData.phone.toString(),
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          )),
                       ElevatedButton(
-                          onPressed: () {
-                            mylaunchURL("https:\/\/www.richmond.gov.uk\/cctv");
-                          },
-                          child:
-                              Text("Press to view Privacy Policy Statement")),
+                        onPressed: () {
+                          mylaunchURL(_currentCCTVData.policyUrl!);
+                        },
+                        child: Text(
+                          "Press to view Privacy Policy Statement",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
                     ],
                   ),
                 ),
@@ -179,20 +347,23 @@ class _HomePageState extends State<HomePage> {
     for (CCTVModel temp in CCTVData) {
       LatLng location = LatLng(temp.lat, temp.long);
       var marker = Marker(
-          markerId: MarkerId(temp.cctv_id.toString()),
+          markerId: MarkerId(temp.cctvId.toString()),
           position: location,
           onTap: () {
             setState(() {
               _infoWindowPosition = 0;
-              _currentCCTVData.cctv_road = temp.cctv_road;
-              _currentCCTVData.cctv_id = temp.cctv_id;
+              _currentCCTVData.cctvRoad = temp.cctvRoad;
+              _currentCCTVData.cctvId = temp.cctvId;
               _currentCCTVData.lat = temp.lat;
               _currentCCTVData.long = temp.long;
+              _currentCCTVData.phone = temp.phone;
+              _currentCCTVData.email = temp.email;
+              _currentCCTVData.policyUrl = temp.policyUrl;
             });
           }
           // icon: markerIcon,
           );
-      _markers[temp.cctv_id.toString()] = marker;
+      _markers[temp.cctvId.toString()] = marker;
     }
 
     setState(() {});
